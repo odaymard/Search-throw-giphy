0c9f39f118e9e1f7a0a687e78478c9aec0aa6c09
oday mardini <odaymard88@gmail.com>
2017-10-12 01:25:47 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,13 +1,9 @@
 
  function CallApi() {
-	var xhttp = new XMLHttpRequest;
-    var gettext=document.getElementById('txtsearch');
+    var xhttp = new XMLHttpRequest;
+       var gettext=document.getElementById('txtsearch');
       var ul=document.querySelector('.flex-container');
 	 ul.innerHTML = '';
-
-
-
-   
    xhttp.open("GET", "https://api.giphy.com/v1/gifs/search?q="  + gettext.value + "&api_key=RMyhQ6NDuKnsfix5ARS4pcA7N4kOxTQi&limit=12", false);
    xhttp.setRequestHeader("Content-type", "application/json");
    xhttp.send();
